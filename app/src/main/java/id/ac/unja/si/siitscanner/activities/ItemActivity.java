e62f169950970e8b4fcce3ed15fd3df8a4330d23
Norman Syarif <norman.syf@gmail.com>
2018-04-30 18:15:18 +0700

    Item page design

--- app/src/main/java/id/ac/unja/si/siitscanner/activities/ItemActivity.java
+++ app/src/main/java/id/ac/unja/si/siitscanner/activities/ItemActivity.java
@@ -1,8 +1,12 @@
 package id.ac.unja.si.siitscanner.activities;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,6 +16,11 @@ import id.ac.unja.si.siitscanner.common.Url;
 
 public class ItemActivity extends AppCompatActivity {
     TextView loc_name_text;
+    TextView sci_name_text;
+    TextView family_text;
+    Button go_to_web_button;
+    String link;
+    // Typeface helvetica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,11 @@ public class ItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item);
 
         loc_name_text = findViewById(R.id.loc_name_text);
+        sci_name_text = findViewById(R.id.sci_name_text);
+        family_text = findViewById(R.id.family_text);
+        go_to_web_button = findViewById(R.id.go_to_web_button);
+
+        // helvetica = Typeface.createFromAsset(getAssets(), "fonts/helvetica.ttf");
 
         getDataFromHome();
     }
@@ -42,7 +56,14 @@ public class ItemActivity extends AppCompatActivity {
         }
     }
 
+    public void OnGoToWebClicked(View v) {
+        Toast.makeText(this, link, Toast.LENGTH_SHORT).show();
+    }
+
     private void showData(String id, String loc_name, String sci_name, String family) {
-        loc_name_text.setText(Url.ITEM_URL + id + loc_name + sci_name + family);
+        loc_name_text.setText(loc_name);
+        sci_name_text.setText(sci_name);
+        family_text.setText(family);
+        link = Url.ITEM_URL + id;
     }
 }
--- app/src/main/java/id/ac/unja/si/siitscanner/common/Url.java
+++ app/src/main/java/id/ac/unja/si/siitscanner/common/Url.java
@@ -1,5 +1,5 @@
 package id.ac.unja.si.siitscanner.common;
 
 public class Url {
-    public static final String ITEM_URL = "http://www.example.com/plant/";
+    public static final String ITEM_URL = "http://www.example.com/species/";
 }
