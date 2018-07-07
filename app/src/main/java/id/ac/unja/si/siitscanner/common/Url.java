584291d28920092630ef15ee14d586fba8e541b2
Norman Syarif <norman.syf@gmail.com>
2018-07-07 17:08:41 +0700

    Update Url.java

--- app/src/main/java/id/ac/unja/si/siitscanner/common/Url.java
+++ app/src/main/java/id/ac/unja/si/siitscanner/common/Url.java
@@ -1,7 +1,7 @@
 package id.ac.unja.si.siitscanner.common;
 
 public class Url {
-    private static String domain = "http://johnsmith393939.000webhostapp.com/";
+    private static String domain = "http://johnsmith393939.000webhostapp.com";
     private static String speciesPath = "/species";
 
     public static String getUrl() {
